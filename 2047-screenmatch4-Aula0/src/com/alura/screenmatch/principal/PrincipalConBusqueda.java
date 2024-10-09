1a24b230115d3921f8e603e7a4f449426b44dc4b
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-10-09 12:21:26 -0500

    Uso de biblioteca Gson

--- 2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ 2047-screenmatch4-Aula0/src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -2,7 +2,9 @@ package com.alura.screenmatch.principal;
 
 import com.alura.screenmatch.modelos.Titulo;
 import com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -15,7 +17,7 @@ public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Escraba el nombre de una pelicula: ");
+        System.out.println("Escriba el nombre de una pelicula: ");
         var pelicula = teclado.nextLine();
 
         String direccion = "https://www.omdbapi.com/?t=" + pelicula + "&apikey=45f25040";
@@ -30,9 +32,10 @@ public class PrincipalConBusqueda {
         String json = response.body();
         System.out.println(json);
 
-        Gson gson = new Gson();
-        //Titulo miTitulo = gson.fromJson(json, Titulo.class);
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(miTituloOmdb);
+        Titulo miTitulo = new Titulo(miTituloOmdb);
+        System.out.println(miTitulo);
     }
 }
